d8b4f3674c4be2f78b3a65a9689d75cae4ea3fe5
Michael Zhou <xiaohuimz@yahoo.com>
2018-05-01 23:33:09 -0400

    Adding locking to mapping repository to ensure integrity of UPDATE

--- src/main/java/com/sas/example/demo/MappingRepository.java
+++ src/main/java/com/sas/example/demo/MappingRepository.java
@@ -1,13 +1,26 @@
 package com.sas.example.demo;
 
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import javax.persistence.LockModeType;
+
 /**
- * Custom repository for string mapping resource that supports paging and sorting
- *
- * TODO - add transaction support
+ * Custom repository for string mapping resource that supports paging and sorting. Paging may be necessary for
+ * reading large number of records.
  */
 @RepositoryRestResource
 public interface MappingRepository extends PagingAndSortingRepository<Mapping, String> {
+
+    // Re-declare save methods to delegate record locking to underlying DB
+
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Mapping save(Mapping entity);
+
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Iterable<Mapping> save(Iterable entities);
+
 }
